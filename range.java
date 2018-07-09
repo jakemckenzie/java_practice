89f1b5fbbf01756de3a84b12cc58ccec245c7af5
jakemckenzie <jake314@uw.edu>
2018-07-08 20:09:57 -0700

    type exception and base case weirdness

--- range.java
+++ range.java
@@ -1,9 +1,10 @@
 public int range(ArrayList list) {
+    if (list.size() == 0) return 0; 
     int min = Integer.MAX_VALUE;
     int max = Integer.MIN_VALUE;
     for (int i = 0; i < list.size(); i++) {
-        min = (list.get(i) < min) ? list.get(i): min;
-        max = (list.get(i) > max) ? list.get(i) : max;
+        min = ((int)list.get(i) < min) ? (int)list.get(i): min;
+        max = ((int)list.get(i) > max) ? (int)list.get(i) : max;
     }
     return (max - min) + 1;
 }
